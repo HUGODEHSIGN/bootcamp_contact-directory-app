bcd5ff89c87a26b80e0fab91113b284312817280
Hugo Hsi <hhsi@hugodehsign.com>
2024-05-01 10:56:37 -0400

    fixed index db code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,53 @@
-// TODO: Install the following package:
+// todos: Install the following package:
 import { openDB } from 'idb';
 
-// TODO: Complete the initDb() function below:
+// todos: Complete the initDb() function below:
 const initdb = async () => {
-
+  openDB('todos', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('todos')) {
+        console.log('todos database already exists');
+        return;
+      }
+      db.createObjectStore('todos', { keyPath: 'id', autoIncrement: true });
+      console.log('todos database created');
+    },
+  });
 };
 
-
-// TODO: Complete the postDb() function below:
-export const postDb = async (name, home, cell, email)  => {
- 
+// todos: Complete the postDb() function below:
+export const postDb = async (name, home, cell, email) => {
+  console.log('POST to the database', name, home, cell, email);
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readwrite');
+  const store = tx.objectStore('todos');
+  const request = store.add({ name, home, cell, email });
+  const result = await request;
+  console.log('Data saved to the database', result);
 };
 
-// TODO: Complete the getDb() function below:
+// todos: Complete the getDb() function below:
 export const getDb = async () => {
-  
+  console.log('GET from database');
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readonly');
+  const store = tx.objectStore('todos');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
-// TODO: Complete the deleteDb() function below:
+// todos: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+  console.log('DELETE from the database', id);
+  const todossDb = await openDB('todoss', 1);
+  const tx = todossDb.transaction('todoss', 'readwrite');
+  const store = tx.objectStore('todoss');
+  const request = store.delete(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,7 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -6,22 +9,65 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
-      cards: './src/js/cards.js'
+      cards: './src/js/cards.js',
     },
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Todo',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        publicPath: './',
+        name: 'Todos',
+        short_name: 'Todos',
+        start_url: './',
+        fingerprints: false,
+        description: 'Awesome Todos List',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     // TODO: Add the correct modules
     module: {
-
-    }
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
   };
 };
